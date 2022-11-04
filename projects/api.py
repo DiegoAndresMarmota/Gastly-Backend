b08353de0e90bd817d5c3ca9fa504a2fa6730fd3
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-11-04 18:13:33 -0300

    Actualización

--- projects/api.py
+++ projects/api.py
@@ -1,9 +1,9 @@
-from .models import project
+from .models import Project
 from rest_framework import viewsets, permissions
 from .secuencia import ProjectSecuencia
 
 class ProjectViewSet(viewsets.ModelViewSet):
-    queryset = project.objects.all()
+    queryset = Project.objects.all()
     permission_classes = [permissions.AllowAny]
     secuencia_class = ProjectSecuencia
 
--- projects/secuencia.py
+++ projects/secuencia.py
@@ -1,10 +1,10 @@
 from rest_framework import secuencia
-from .models import project
+from .models import Project
 
 
 class ProjectSecuencia(secuencia.ModelSecuencia):
     class Meta:
-        model = project
+        model = Project
         fields = ('id', 'titulo', 'descripcion',
                   'detalles', 'stock', 'precios')
         read_only_fields = ('detalles', )
