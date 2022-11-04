86b8e2d8728512aae24c3109e23551260c24896e
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-11-04 17:25:51 -0300

    class Project(models)

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    detalles = models.CharField(max_length=200)
+    stock = models.IntegerField()
+    precios = models.IntegerField()
